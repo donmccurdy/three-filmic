c53a691f7a71171eeae8929a47aa576139333648
Don McCurdy <dm@donmccurdy.com>
2022-05-08 23:21:26 -0400

    Clean up

--- src/FilmicPass.ts
+++ src/FilmicPass.ts
@@ -75,58 +75,54 @@ export class FilmicPass extends EffectPass {
 
 	// TODO(cleanup): Do without a build method?
 	recompile() {
-		// Remove current filmic transform.
+		// Remove previous filmic transform.
 		this.effects.length = this._prevEffects.length;
 
+		// Apply updated filmic transform.
+
+		// 1. Exposure.
 		this.effects.push(this._exposureTransform);
 
 		if (this._view !== View.NONE) {
 
-			// # Scene Linear to Filmic Log
+			// 2. Scene Linear to Filmic Log
 			this.effects.push(
 				new AllocationTransform({
 					allocation: Allocation.LG2,
-					domain: new Vector2(-12.473931188, 12.526068812)
+					domain: new Vector2(-12.473931188, 12.526068812),
 				}),
 				new LUTEffect(this.filmicLUT),
 				new AllocationTransform({
 					allocation: Allocation.UNIFORM,
-					domain: new Vector2(0, 0.66)
+					domain: new Vector2(0, 0.66),
 				}),
 			);
 
-			// # Look Transform
+			// 3. Look Transform
 			if (this._view === View.FILMIC
-				|| this._view === View.GRAYSCALE
-				|| this._view === View.FALSE_COLOR) {
-				this.effects.push(
-					new LUT1DEffect(this._lookLUT),
-				);
+				|| this._view === View.GRAYSCALE) {
+				this.effects.push(new LUT1DEffect(this._lookLUT));
 			}
 
-			// # View Transform
+			// 4. View Transform
+			if (this._view === View.GRAYSCALE
+				|| this._view === View.FALSE_COLOR) {
+				this.effects.push(
+					new MatrixTransform(new Matrix4().fromArray([
+						0.2126729, 0.7151521, 0.0721750, 0,
+						0.2126729, 0.7151521, 0.0721750, 0,
+						0.2126729, 0.7151521, 0.0721750, 0,
+						0, 0, 0, 1
+					])),
+				);
+			}
 			if (this._view === View.FALSE_COLOR) {
-				this.effects.push(
-					new MatrixTransform(new Matrix4().fromArray([
-						0.2126729, 0.7151521, 0.0721750, 0,
-						0.2126729, 0.7151521, 0.0721750, 0,
-						0.2126729, 0.7151521, 0.0721750, 0,
-						0, 0, 0, 1
-					])),
-					new LUTEffect(this._falseColorLUT),
-				);
-			} else if (this._view === View.GRAYSCALE) {
-				this.effects.push(
-					new MatrixTransform(new Matrix4().fromArray([
-						0.2126729, 0.7151521, 0.0721750, 0,
-						0.2126729, 0.7151521, 0.0721750, 0,
-						0.2126729, 0.7151521, 0.0721750, 0,
-						0, 0, 0, 1
-					])),
-				);
+				this.effects.push(new LUTEffect(this._falseColorLUT));
 			}
 		}
 
+		// Look Transforms output to sRGB. When no Look is applied, include the
+		// default output encoding.
 		this.fullscreenMaterial.encodeOutput = this._view === View.NONE;
 
 		if (this.renderer) super.recompile();
