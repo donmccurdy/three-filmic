b9869979ace13df698e09ed7d20548db1894c963
Don McCurdy <dm@donmccurdy.com>
2022-05-08 23:43:47 -0400

    Clean up MatrixTransform

--- src/effects/MatrixTransform.ts
+++ src/effects/MatrixTransform.ts
@@ -13,18 +13,13 @@ void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor)
 }
 	`.trim();
 
+	readonly matrix: Matrix4;
+
 	constructor(matrix: Matrix4) {
 		super('MatrixTransform', MatrixTransform.FRAG, {
 			blendFunction: BlendFunction.NORMAL,
 			uniforms: new Map([[Uniforms.MATRIX, new Uniform(matrix)]]),
 		});
-	}
-
-	get matrix(): number {
-		return this.uniforms.get(Uniforms.MATRIX)!.value;
-	}
-
-	set matrix(matrix: number) {
-		this.uniforms.get(Uniforms.MATRIX)!.value = matrix;
+		this.matrix = matrix;
 	}
 }
